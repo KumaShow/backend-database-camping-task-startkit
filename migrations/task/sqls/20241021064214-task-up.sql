6a66101fde72876fd0df2e552283d63d4ef46257
Benson <kumadth@gmail.com>
2024-12-06 00:30:32 +0800

    [Modify] 移除子查詢內分號

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -94,19 +94,19 @@ VALUES
 		(SELECT id FROM "USER" WHERE email = 'wXlTq@hexschooltest.io'),
 		(SELECT id FROM "CREDIT_PACKAGE" WHERE "name" = '14 堂組合包方案'),
 		(SELECT credit_amount FROM "CREDIT_PACKAGE" WHERE "name" = '14 堂組合包方案'),
-		(SELECT price FROM "CREDIT_PACKAGE" WHERE "name" = '14 堂組合包方案');
+		(SELECT price FROM "CREDIT_PACKAGE" WHERE "name" = '14 堂組合包方案')
 	),
 	(	
 		(SELECT id FROM "USER" WHERE email = 'wXlTq@hexschooltest.io'),
 		(SELECT id FROM "CREDIT_PACKAGE" WHERE "name" = '21 堂組合包方案'),
 		(SELECT credit_amount FROM "CREDIT_PACKAGE" WHERE "name" = '21 堂組合包方案'),
-		(SELECT price FROM "CREDIT_PACKAGE" WHERE "name" = '21 堂組合包方案');
+		(SELECT price FROM "CREDIT_PACKAGE" WHERE "name" = '21 堂組合包方案')
 	),
 	(
 		(SELECT id FROM "USER" WHERE email = 'richman@hexschooltest.io'),
 		(SELECT id FROM "CREDIT_PACKAGE" WHERE "name" = '14 堂組合包方案'),
 		(SELECT credit_amount FROM "CREDIT_PACKAGE" WHERE "name" = '14 堂組合包方案'),
-		(SELECT price FROM "CREDIT_PACKAGE" WHERE "name" = '14 堂組合包方案');
+		(SELECT price FROM "CREDIT_PACKAGE" WHERE "name" = '14 堂組合包方案')
 	);
 
 
